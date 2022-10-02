6742e7322fd1bab67325ed593b50a27e4ae515a4
william41301 <114844950+william41301@users.noreply.github.com>
2022-10-02 11:15:37 +0700

    Kalkulator sederhana.cpp

---
+++ .cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	int a, b, hasil;
+	char Operator;
+
+	cout << "Masukkan bilangan pertama = ";
+	cin >> a;
+	cout << "Masukkan operator = ";
+	cin >> Operator;
+	cout << "Masukkan bilangan kedua = ";
+	cin >> b;
+
+	if (Operator == '+') {
+		hasil = a + b;
+		cout << "hasil = " << hasil << endl;
+		cin >> hasil;
+		}else if (Operator == '-') {
+			hasil = a - b;
+			cout << " hasil = " << hasil << endl;
+			cin >> hasil;
+		}else if (Operator == '*') {
+			hasil = a * b;
+			cout << "hasil = " << hasil << endl;
+			cin >> hasil ;
+		}else if (Operator == '/') {
+			hasil = a / b;
+			cout << "hasil = " << hasil << endl;
+			cin >> hasil;
+		}
+
+	
+	cin.get();
+	return 0; 
+}
